cf0f5b3d6e46e11e6b7bbd20915e39ce20b85f3a
desarrollo789 <43588250+desarrollo789@users.noreply.github.com>
2020-01-20 02:52:18 -0800

    image loader
    solve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicImageLoader } from 'ionic-image-loader';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicImageLoader.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
